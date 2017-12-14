2203482547ad8af41e0f093b675fc9b74f4a8504
Heark <adtune6509@student.waynecc.edu>
2017-12-14 01:59:00 -0500

    Update main.js

--- main.js
+++ main.js
@@ -139,7 +139,7 @@ Tyrone.on('message', message => {
                 }
                 break;
             case 'leave':
-                Tyrone.voiceChannel.leave();
+                Tyrone.user.voiceChannel.leave();
                 break;
             case 'soundboard':
                 var sound_name = commanddata;
