ce3efc6691d46314e944b4c2e2f34d0685876247
Heark <adtune6509@student.waynecc.edu>
2017-12-14 01:13:41 -0500

    Update main.js

--- main.js
+++ main.js
@@ -55,108 +55,136 @@ function load_error(error) {
 }
 
 Tyrone.on('message', message => {
-            /*  if (message.content === 'ping') {
-                message.reply('pong');
-              }
-            */
-            // Detect if the message is a commmand.
-            if (message.content.toLowerCase().charAt(0) == "$") {
-                // gets the command used name. For example if the user types $hi this variable will contain "hi"
-                var command = message.content.split("$").pop().split(' ').shift();
-                // gets the user input after the command
-                var commandinit = message.content.toLowerCase().replace("$" + command, "").split(" / ", 2);
-                var commanddata = commandinit[0];
-                var commanddata2 = commandinit[1];
-                // find out witch command is being used and write the code for it
-                switch (command) {
-                    // begin define command
-                    case 'define':
-                        var word = commanddata;
-                        getJSON("http://api.urbandictionary.com/v0/define?term=" + word, function(error, c) {
-                            if (error) {
-                                throw Error(error);
+    /*  if (message.content === 'ping') {
+        message.reply('pong');
+      }
+    */
+    // Detect if the message is a commmand.
+    if (message.content.toLowerCase().charAt(0) == "$") {
+        // gets the command used name. For example if the user types $hi this variable will contain "hi"
+        var command = message.content.split("$").pop().split(' ').shift();
+        // gets the user input after the command
+        var commandinit = message.content.toLowerCase().replace("$" + command, "").split(" / ", 2);
+        var commanddata = commandinit[0];
+        var commanddata2 = commandinit[1];
+        // find out witch command is being used and write the code for it
+        switch (command) {
+            // begin define command
+            case 'define':
+                var word = commanddata;
+                getJSON("http://api.urbandictionary.com/v0/define?term=" + word, function(error, c) {
+                    if (error) {
+                        throw Error(error);
 
-                            } else {
-                                if (c.list[0] == undefined) {
-                                    message.reply("nigga stfu that don't mean shit.");
-                                } else {
-                                    var getword = c.list[0].word,
-                                        word = getword.charAt(0).toUpperCase() + getword.substring(1),
-                                        definition = c.list[0].definition,
-                                        out = "" + word + ": " + definition;
+                    } else {
+                        if (c.list[0] == undefined) {
+                            message.reply("nigga stfu that don't mean shit.");
+                        } else {
+                            var getword = c.list[0].word,
+                                word = getword.charAt(0).toUpperCase() + getword.substring(1),
+                                definition = c.list[0].definition,
+                                out = "" + word + ": " + definition;
 
-                                    message.reply(out);
-                                }
-
-                            }
-                        });
-                        break;
-
-                    case 'iplocation':
-                        var ipadd = commanddata;
-                        getJSON("http://freegeoip.net/json/" + ipadd, function(error, c) {
-                            if (error) {
-                                message.reply("what fool?");
-                            } else {
-                                if (c.ip == undefined) {
-                                    message.reply("what fool?");
-                                } else {
-                                    message.reply("Country: " + c.country_name + " City: " + c.city + ", " + c.region_name + " " + c.zip_code);
-                                }
-                            }
-                        });
-                        break;
-                    case 'insult':
-                        var insulteename = commanddata;
-                        message.channel.send(insulteename + " " + insults[Math.floor(Math.random() * insults.length)]);
-
-                        break;
-                    case 'test':
-                        message.channel.send("test command data 1: " + commanddata + " command data 2: " + commanddata2);
-                        break;
-
-                    case 'tweet':
-                        var params = {
-                            screen_name: commanddata
-                        };
-                        TwitterClient.get('statuses/user_timeline', params, function(error, tweets, response) {
-                                if (!error) {
-                                    message.channel.send(commanddata +"'s latest tweet says: \"" + tweets[0].text + " \" with " + tweets[0].favorite_count + " likes and " + tweets[0].retweet_count + " retweets.");
-                                    }
-                                    else {
-                                        message.reply("I'm sorry, what?")
-                                    }
-                                });
-                            break;
-                            // if the command isn't listed above return this message 
-                            default:
-                            message.reply("That command doesn't exist retard");
+                            message.reply(out);
                         }
+
+                    }
+                });
+                break;
+
+            case 'iplocation':
+                var ipadd = commanddata;
+                getJSON("http://freegeoip.net/json/" + ipadd, function(error, c) {
+                    if (error) {
+                        message.reply("what fool?");
+                    } else {
+                        if (c.ip == undefined) {
+                            message.reply("what fool?");
+                        } else {
+                            message.reply("Country: " + c.country_name + " City: " + c.city + ", " + c.region_name + " " + c.zip_code);
+                        }
+                    }
+                });
+                break;
+            case 'insult':
+                var insulteename = commanddata;
+                message.channel.send(insulteename + " " + insults[Math.floor(Math.random() * insults.length)]);
+
+                break;
+            case 'test':
+                message.channel.send("test command data 1: " + commanddata + " command data 2: " + commanddata2);
+                break;
+
+            case 'tweet':
+                var params = {
+                    screen_name: commanddata
+                };
+                TwitterClient.get('statuses/user_timeline', params, function(error, tweets, response) {
+                    if (!error) {
+                        message.channel.send(commanddata + "'s latest tweet says: \"" + tweets[0].text + " \" with " + tweets[0].favorite_count + " likes and " + tweets[0].retweet_count + " retweets.");
+                    } else {
+                        message.reply("I'm sorry, what?")
+                    }
+                });
+                break;
+            case 'join':
+                if (message.member.voiceChannel) {
+                    message.member.voiceChannel.join()
+                        .then(connection => {
+                            message.reply('Yo i\'m here.');
+                        })
+                        .catch(console.log);
+                } else {
+                    message.reply('You\'re not even in a voice channel :/');
                 }
-            });
+                break;
+            case 'leave':
 
-        Tyrone.login(process.env.TOKEN);
+                break;
+            case 'soundboard':
+                var sound_name = commanddata;
+                switch (sound_name) {
 
-        // Web app (Express + EJS)
-        const http = require('http');
-        const express = require('express');
-        const app = express();
+                    case 'bruh':
+                        const dispatcher = connection.playArbitraryInput('https://sound.peal.io/ps/audios/000/000/269/original/bruh?1469744327')
+                        break;
 
-        const port = process.env.PORT || 5000;
+                    default:
+                        message.reply('Tf is that?');
 
-        app.set('view engine', 'ejs');
 
-        app.use(express.static(__dirname + '/public'));
+                }
+                break;
 
-        app.get('/', (request, response) => {
-            response.render('index');
-        });
+                // if the command isn't listed above return this message 
+            default:
+                message.reply("That command doesn't exist retard");
+        }
+    }
+});
 
-        app.listen(port, () => {
-            console.log('Tyrone is running on http://localhost:' + port);
-        });
+Tyrone.login(process.env.TOKEN);
 
-        setInterval(() => {
-            http.get('https://tyroneload.herokuapp.com');
-            console.log("Server pinged");
-        }, 900000);
+// Web app (Express + EJS)
+const http = require('http');
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (request, response) => {
+    response.render('index');
+});
+
+app.listen(port, () => {
+    console.log('Tyrone is running on http://localhost:' + port);
+});
+
+setInterval(() => {
+    http.get('https://tyroneload.herokuapp.com');
+    console.log("Server pinged");
+}, 900000);
